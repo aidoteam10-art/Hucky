712f2a2b712999da647f3667b15010b5e8bd123d
Osipova Olesia <osipolesia13@gmail.com>
2026-04-18 20:49:06 +0300

    Додано адаптивнiсть до сторiнки опису завдання турнiру

--- frontend/src/routes/(app)/tournaments/tourtask/+page.svelte
+++ frontend/src/routes/(app)/tournaments/tourtask/+page.svelte
@@ -17,72 +17,73 @@
   }
 </script>
 
-<div class="max-w-7xl mx-auto mt-10 pb-21 space-y-6">
+<div class="max-w-7xl mx-auto mt-10 px-6 md:px-10 xl:px-0 pb-21 space-y-6 font-sans">
 
-  <div class="bg-[#191F00] text-white rounded-3xl p-15 pl-21 pr-14 mb-12.5 flex justify-between items-center">
-    <div>
-      <h2 class="text-lg font-bold mb-12 text-[1.5rem]">{tournament.round}</h2>
-      <a href="/task_submission" class="text-[1rem] font-bold bg-linear-to-r from-[#BCEB01] to-[#EEFF00] mt-[16px] text-[#191F00] px-7.5 py-4 rounded-lg hover:brightness-90 transition-all">
+  <div class="bg-[#191F00] text-white rounded-3xl p-8 md:p-12 lg:p-15 lg:pl-21 lg:pr-14 mb-12.5 flex flex-col md:flex-row justify-between items-center gap-8 text-center md:text-left">
+    <div class="w-full md:w-auto">
+      <h2 class="text-xl md:text-[1.5rem] font-bold mb-8 md:mb-12">{tournament.round}</h2>
+      <a href="/task_submission" class="inline-block text-[1rem] font-bold bg-linear-to-r from-[#BCEB01] to-[#EEFF00] text-[#191F00] px-9 py-3.5 rounded-lg hover:brightness-90 transition-all w-full md:w-auto text-center">
         Здати роботу
       </a>
     </div>
 
-    <div class="text-right">
-      <p class="font-regular text-[1rem]">Часу залишилося</p>
-      <p class="text-[2.5rem] font-semibold mt-5">{timeLeft}</p>
+    <div class="md:text-right">
+      <p class="font-regular text-[1rem] opacity-80">Часу залишилося</p>
+      <p class="text-3xl md:text-[2.5rem] font-semibold mt-3 md:mt-5 tracking-tight md:tracking-normal">{timeLeft}</p>
     </div>
   </div>
 
-  <div class="border border-[#B4B4B4] rounded-2xl px-7.5 py-9 bg-white ">
+  <div class="border border-[#B4B4B4] rounded-2xl px-6 lg:px-7.5 py-7 lg:py-9 bg-white">
     <div class="flex items-center gap-2 mb-3 text-gray-900 font-medium">
-      <img src="/icons/task_desc.svg" alt="task_description">
+      <img src="/icons/task_desc.svg" alt="task_description" class="w-5 h-5 lg:w-auto">
       <span class='font-semibold text-[#191F00]'>Опис завдання</span>
     </div>
-    <p class="text-[191F00] font-regular text-[1rem]">{tournament.taskDescription}</p>
+    <p class="text-[#191F00] font-regular text-[1rem] leading-relaxed">{tournament.taskDescription}</p>
   </div>
 
-  <div class="border border-[#B4B4B4] rounded-2xl px-7.5 py-9 bg-white ">
+  <div class="border border-[#B4B4B4] rounded-2xl px-6 lg:px-7.5 py-7 lg:py-9 bg-white">
     <div class="flex items-center gap-2 mb-3 text-gray-900 font-medium">
       <span class='font-semibold text-[#191F00]'>Необхідні технології</span>
     </div>
-    <p class="text-[191F00] font-regular text-[1rem]">{tournament.technologies}</p>
+    <p class="text-[#191F00] font-regular text-[1rem] leading-relaxed">{tournament.technologies}</p>
   </div>
 
-  <div class="border border-[#B4B4B4] rounded-2xl px-7.5 py-9 bg-white">
-    <div class="flex items-center gap-2 mb-8 text-gray-900 font-medium">
-      <img src="/icons/required.svg" alt="required">
+  <div class="border border-[#B4B4B4] rounded-2xl px-6 lg:px-7.5 py-7 lg:py-9 bg-white">
+    <div class="flex items-center gap-2 mb-6 lg:mb-8 text-gray-900 font-medium">
+      <img src="/icons/required.svg" alt="required" class="w-5 h-5 lg:w-auto">
       <span class='font-semibold text-[#191F00]'>Обов’язкові вимоги</span>
     </div>
 
-    {#each tournament.mandatoryRequirements as requirement, i}
-      <div class="bg-[#CCFF00] text-[#191F00] rounded-xl p-5 mb-3 text-[1rem] font-regular">
-        <div class="flex items-center gap-4">
-            <span class="w-8 h-8 flex items-center justify-center border border-current font-regular rounded-lg text-[1rem]">
-              {i + 1}
-            </span>
-
-          <p class="font-regular text-[16px]">
-            {requirement}
-          </p>
+    <div class="space-y-3">
+      {#each tournament.mandatoryRequirements as requirement, i}
+        <div class="bg-[#CCFF00] text-[#191F00] rounded-xl p-4 lg:p-5 text-[1rem] font-regular">
+          <div class="flex items-center gap-4">
+              <span class="shrink-0 w-8 h-8 flex items-center justify-center border border-[#191F00]/20 font-regular rounded-lg text-[1rem]">
+                {i + 1}
+              </span>
+            <p class="font-regular text-[16px]">
+              {requirement}
+            </p>
+          </div>
         </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
   </div>
 
-  <div class="border border-[#B4B4B4] rounded-2xl px-7.5 py-9 bg-white mb-4">
-    <div class="flex items-center gap-3 mb-5 text-gray-900 font-medium">
-      <img src="/icons/schedule.svg" alt="schedule">
+  <div class="border border-[#B4B4B4] rounded-2xl px-6 lg:px-7.5 py-7 lg:py-9 bg-white mb-4">
+    <div class="flex items-center gap-3 mb-6 lg:mb-8 text-gray-900 font-medium">
+      <img src="/icons/schedule.svg" alt="schedule" class="w-5 h-5 lg:w-auto">
       <span class='font-semibold text-[#191F00]'>Розклад</span>
     </div>
-    <div class="flex w-full">
+    <div class="flex flex-col sm:flex-row w-full gap-8 sm:gap-0">
       <div class="flex-1 text-left">
-        <h3 class = "font-semibold text-[1.2rem] text-[#696969] mb-2">Початок</h3>
-        <p class="font-semibold text-[#191F00] text-[1.2rem] tracking-[0.15em]">21.03.2026, 10:00</p>
+        <h3 class="font-semibold text-[1.1rem] lg:text-[1.2rem] text-[#696969] mb-2">Початок</h3>
+        <p class="font-semibold text-[#191F00] text-[1.1rem] lg:text-[1.2rem] tracking-[0.1em] lg:tracking-[0.15em]">{tournament.startDate}</p>
       </div>
 
       <div class="flex-1 text-left">
-        <h3 class = "font-semibold text-[1.2rem] text-[#696969] mb-2">Дедлайн</h3>
-        <p class="font-semibold text-[#191F00] text-[1.2rem] tracking-[0.15em]">30.04.2026, 17:00</p>
+        <h3 class="font-semibold text-[1.1rem] lg:text-[1.2rem] text-[#696969] mb-2">Дедлайн</h3>
+        <p class="font-semibold text-[#191F00] text-[1.1rem] lg:text-[1.2rem] tracking-[0.1em] lg:tracking-[0.15em]">{tournament.endDate}</p>
       </div>
     </div>
   </div>
